e15a37c152dc91f94110cdb109305065b117857c
Kara LeSueur <kara.lesueur@gmail.com>
2020-08-18 15:01:19 -0700

    FilterString

---
+++ src/components/Topics/FilterString.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react' 
+
+class FilterString extends Component {
+
+    render() {
+        return (
+            <p> FilterString Component </p>
+        )
+    }
+}
+
+export default FilterString
