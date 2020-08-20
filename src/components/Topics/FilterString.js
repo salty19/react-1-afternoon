537cfe5b0e9c2aca03a6bf03e753051ae6809df6
Kara LeSueur <kara.lesueur@gmail.com>
2020-08-19 17:21:44 -0700

    button-updated

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -12,14 +12,21 @@ class FilterString extends Component {
         }
     }
 
+    handleChange(val) {
+        this.setState({ userInput: val })
+    }
+
+    filterNames(userInput) {
+        
+    }
+
     render() {
         return (
             <div className="puzzleBox filterStringPB">
                 <h4> Filter String </h4>
-                <span className="puzzleText"> Names:  {JSON.stringify(this.state.names, null, 10)}
-                </span>
-                <input className="inputLine" />
-                <button className="confirmationButton"> Filtered </button>
+                <span className="puzzleText"> Names:  {JSON.stringify(this.state.names, null, 10)}</span>
+                <input className="inputLine" onChange={(e) => this.handleChange(e.target.value)} />
+                <button className="confirmationButton" onClick={() => this.filterNames(this.state.userInput)}> Filtered </button>
                 <span className="resultsBox filterStringRB"> Filtered Names:  {JSON.stringify(this.state.filteredNames, null, 10)}
                 </span>
             </div>
