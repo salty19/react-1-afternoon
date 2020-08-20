89824cf35b887ceea63f265cb46b40ef4b265327
Kara LeSueur <kara.lesueur@gmail.com>
2020-08-19 17:09:14 -0700

    this.state

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -2,9 +2,25 @@ import React, {Component} from 'react'
 
 class FilterString extends Component {
 
+    constructor() {
+        super()
+
+        this.state = {
+            names: ['James', 'Jessica', 'Melody', 'Tyler', 'Blake', 'Jennifer', 'Mark', 'Maddy'],
+            userInput: '',
+            filteredNames: []
+        }
+    }
+
     render() {
         return (
-            <p> FilterString Component </p>
+            <div className="puzzleBox filterStringPB">
+                <h4> Filter String </h4>
+                <span className="puzzleText"></span>
+                <input className="inputLine" />
+                <button className="confirmationButton"> Filtered </button>
+                <span className="resultsBox filterStringRB"></span>
+            </div>
         )
     }
 }
