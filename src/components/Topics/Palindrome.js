ee3252243d0ed08267cc2b8a1d38b51c8ed6aaa4
Kara LeSueur <kara.lesueur@gmail.com>
2020-08-19 17:41:50 -0700

    button-inProgress

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -2,9 +2,31 @@ import React, {Component} from 'react'
 
 class Palindrome extends Component {
 
+    constructor() {
+        super()
+
+        this.state = {
+            userInput: '',
+            palindrome: ''
+        }
+    }
+
+    handleChange(val) {
+        this.setState({ userInput: val })
+    }
+
+    isPalindrome(userInput) {
+        
+    }
+
     render() {
         return (
-            <p> Palindrome Component </p>
+            <div className="puzzleBox filterStringPB">
+                <h4> Palindrome </h4>
+                <input className="inputLine" onChange={(e) => this.handleChange(e.target.value)} />
+                <button className="confirmationButton" onClick="" ></button>
+                <span className="resultsBox"> Palindrome: {this.state.palindrome} </span>
+            </div>
         )
     }
 }
