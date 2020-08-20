e5c3f6566531bea0a715c069a8db377b2534cced
Kara LeSueur <kara.lesueur@gmail.com>
2020-08-19 18:06:14 -0700

    updateNumbers-for-inputs

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -2,9 +2,33 @@ import React, {Component} from 'react'
 
 class Sum extends Component {
 
+    constructor() {
+        super()
+
+        this.state = {
+            number1: 0,
+            number2: 0,
+            sum: null
+        }
+    }
+
+    updateNumber1(val) {
+        this.setState({ number1: parseInt(val, 10) })
+    }
+
+    updateNumber2(val) {
+        this.setState({ number2: parseInt(val, 10) })
+    }
+
     render() {
         return (
-            <p> Sum Component </p>
+            <div className="puzzlebox sumPB">
+                <h4> Sum </h4>
+                <input className="inputLine" type="number"  onChange={ (e) => this.updateNumber1(e.target.value)} />
+                <input className="inputLine" type="number" onChange={ (e) => this.updateNumber2(e.target.value)} />
+                <button className="confirmationButton"> Add </button>
+                <span className="resultsBox"> Sum: {this.state.sum} </span>
+            </div>
         )
     }
 }
